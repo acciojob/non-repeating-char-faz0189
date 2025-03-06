abdeb5f2654492d4b50369a90248748e5eee15d1
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-07 02:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-
-
-function firstNonRepeatedChar(str) {
-  // Loop through each character in the string
-  for (let i = 0; i < str.length; i++) {
-    // Check if the character appears only once using indexOf and lastIndexOf
-    if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) {
-      return str[i]; // Return the first non-repeated character
-    }
-  }
-  return null; // Return null if no non-repeated character is found
+function firstNonRepeatedChar(str)
+{
+	for (let p = 0;  p<=str.length; p++){
+		if (str.indexof(str[p] === str.lastIndexof(str[p]))){
+			return str[p]
+		}
+	}
+	return null;
 }
