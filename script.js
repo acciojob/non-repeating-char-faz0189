80eb2bcc37a96321ea191259cc440e7bc38cbcfe
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-07 18:08:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 function firstNonRepeatedChar(str)
 {
-	for (let p = 0;  p<=str.length; p++){
-		if (str.indexof(str[p] === str.lastIndexof(str[p]))){
-			return str[p]
+	let map = new Map()
+	for (let  i of str){
+		if(map.has(i)){
+			map.set(i,map.get(i)+1)
+		}else{
+			map.set(i,1)
 		}
+		
 	}
-	return null;
+	let answer = null
+	map.forEach((v,k)=>{
+        // console.log(k,v)
+		if (v==1){
+            answer = k
+			return k
+            
+           
+		}
+			
+	})
+	return answer;
+	
 }
+console.log(firstNonRepeatedChar('aabbcdd'))
